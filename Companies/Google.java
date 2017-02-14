db0eb02116887b3e3aa701c8f7c8370531b6bc35
Poojabafnaprakash <poojabafnaprakash@gmail.com>
2017-02-13 16:53:35 -0800

    added matching pair using stack

---
+++ Companies/Google.java
@@ -0,0 +1,21 @@
+package Companies;
+
+//Please use this Google doc to code during your interview. To free your hands for coding, we recommend that you use a headset or a phone with speaker option.
+//
+//Implement method:
+//List<Range> getRanges(List<Shard> shards, List<Key> keys)
+//That returns list of ranges. Each range represents multiple keys aggregated over a shard:
+//n-keys -> 1-shard -> 1-range
+//
+//Method should return no more than 1 range per shard that spans all keys or their parts belonging to this shard.
+//
+//Each of the `Range`, `Shard` and `Key` classes have `end` and `start` fields of int type, where `start` is inclusive and `end` is exclusive.
+//
+//Example:
+//1-9, 12-59, 100-999 <- shards (input)
+//2-3, 6-8, 11-20, 200-220 <- keys (input)
+//2-8, 12-20, 200-220 <- ranges (output)
+
+public class Google {
+
+}
---
+++ Strings/MatchingPair.java
@@ -0,0 +1,32 @@
+package Strings;
+
+import java.util.Stack;
+
+public class MatchingPair {
+	public int matching(String s){
+		int pos = -1;
+		Stack<Character> stack = new Stack<Character>();
+		stack.push(s.charAt(0));
+		for(int i=1; i<s.length(); i++){
+			if(Character.isLowerCase(s.charAt(i)) && Character.isUpperCase(stack.peek())){
+				if(Character.toLowerCase(s.charAt(i)) == Character.toLowerCase(stack.peek())){
+					stack.pop();
+					pos = i;
+				}
+				else{
+					break;
+				}
+			}
+			else{
+				stack.push(s.charAt(i));
+			}
+		}
+		return pos;
+	}
+	
+	
+	public static void main(String args[]){
+		MatchingPair obj = new MatchingPair();
+		System.out.println(obj.matching("ABba"));
+	}
+}
